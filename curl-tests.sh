684821959068e57675d701ac9e4d7a6fc03c328a
rollen <rafal.wartalski@gmail.com>
2015-01-05 20:11:57 +0200

    Custom Sender Sms

--- curl-tests.sh
+++ curl-tests.sh
@@ -7,7 +7,9 @@ NC='\033[0m'
 assert_equal(){
     if [ $1 != $2 ]
         then
-            echo ${RED}[NOT OK] - Expected to get \"$1\" . Got: \"$2\" ${NC}
+            echo ${RED}[NOT OK] - $3
+            echo Expected: \"$1\" .
+            echo Got: \"$2\" ${NC}
         else
             echo ${GREEN}[OK] - $3${NC}
     fi
@@ -35,6 +37,19 @@ assert_equal "pong" ${ping} "ping"
 unknown_resource=`curl -s -o /dev/null -w "%{http_code}" ${base_address}/unknown_resource -X POST`
 
 assert_equal 404 ${unknown_resource} "unknown resuorce"
+
+#custom_sender_sms
+custom_sender_sms=`curl -s ${base_address}/sms.do \
+    --data format=json \
+    --data encoding=utf-8 \
+    --data username=registered \
+    --data password=200f200f200f200f200f200f200f200f \
+    --data from=Sender \
+    --data to=600000000 \
+    --data-urlencode message='Zażółć gęślą jaźń' \
+    2>&1`
+
+assert_equal '{"count":1,"list":[{"id":"1431037279135587730","points":0.15,"number":"48600000000","submitted_number":"600000000","status":"QUEUE","error":null,"idx":null}]}' ${custom_sender_sms} "custom_sender_sms"
 
 #default_sender_sms
 default_sender_sms=`curl -s ${base_address}/sms.do \
